80bb19688fca2ced4769b726511c9c8313281e1c
johnson-alan <allendotjohnson@gmail.com>
2023-10-04 17:24:55 -0400

    try next.config.js change

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  target: 'serverless',
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'upload.wikimedia.org' }],
   },
+  experimental: {
+    outputFileTracing: true
+  },
+  serverlessFunctions: {
+    'pages/api/ai.js': {
+      outputFileTracingIncludes: ['types/filters.ts']
+    }
+  }
 };
 
 module.exports = nextConfig;
